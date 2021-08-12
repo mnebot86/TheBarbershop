27e4dc918c995650b3b7f5ad13536e0c14f936ea
mnebot <miguelnbt@icloud.com>
2021-08-12 10:28:37 -0400

    created links and routes for all screens

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,51 @@
-
+import { Route } from 'react-router';
+import Layout from './components/Layout/Layout';
+import Splash from './screens/Splash/Splash';
+import SignUp from "./screens/SignUp/SignUp";
+import SignIn from "./screens/SignIn/SignIn";
+import Home from "./screens/Home/Home";
+import Services from "./screens/Services/Services";
+import Detail from "./screens/Detail/Detail";
+import Booking from "./screens/Booking/Booking";
+import Confirmation from "./screens/Confirmation/Confirmation";
+import BookingEdit from "./screens/BookingEdit/BookingEdit"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Let GOOOOOOO!</h1>
-    </div>
+    <Layout>
+      <Route exact path="/">
+        <Splash/>
+      </Route>
+      <Route exact path="/signup">
+        <SignUp/>
+      </Route>
+      <Route exact path="/login">
+        <SignIn/>
+      </Route>
+      <Route exact path="/home">
+        <Home />
+      </Route>
+      <Route exact path="/services">
+        <Services/>
+      </Route>
+      <Route exact path="/detail/:id">
+        <Detail/>
+      </Route>
+      <Route exact path="/booking">
+        <Booking/>
+      </Route>
+      <Route exact path="/confirmation">
+        <Confirmation/>
+      </Route>
+      <Route exact path="/bookingedit/:id">
+        <BookingEdit />
+      </Route>
+      
+
+
+
+    </Layout>
   );
 }
 
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import { Link } from "react-router-dom"
 
 const Nav = () => {
   return (
     <div>
-      
+      <Link to="/home">Home</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/confirmation">Confirmation</Link>
+      <Link to="/signup">Sign Up</Link>
+      <Link to="/login">Login</Link>
+      <Link to="signout">Sign Out</Link>
     </div>
   );
 };
