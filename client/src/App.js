0b109577f24e1a55d72870bb227b99f31b21f788
mnebot <miguelnbt@icloud.com>
2021-08-12 09:43:17 -0400

    added all folder for screens and components. Also services folder with apiConfig.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let GOOOOOOO!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/screens/Splash/Splash.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Splash(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Splash;
