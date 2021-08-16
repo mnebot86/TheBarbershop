210dffd73d42ae784b910d0a17e3ab0c572a86cd
mnebot <miguelnbt@icloud.com>
2021-08-15 20:37:52 -0400

    starting on confirmation screen

--- client/src/screens/Confirmation/Confirmation.jsx
+++ client/src/screens/Confirmation/Confirmation.jsx
@@ -1,26 +1,29 @@
 import { useParams, Link } from "react-router-dom";
-
+import Layout from "../../components/Layout/Layout";
 
 const Confirmation = (props) => {
+  const booking = {
+    date: new Date(),
+    client: props.client?.name,
+    service: props.service?.name,
+  };
+  console.log("booking", booking.date);
 
-  const booking = ({
-  date: new Date(),
-  client: props.client?.name,
-  service: props.service?.name,
-})
-console.log('booking',booking.date)
-
-let { id } = useParams()
+  let { id } = useParams();
 
   return (
-    <div>
-    <img src={props.client?.image_url} alt={props.client?.name} />
-    <p>Name: {booking.client}</p>
-    <p>Appointment: {booking.date?.toString()}</p>
-    <p>Service: {booking?.service}</p>
-    <Link to = {`/booking/${id}/edit`}>
-    <button>Edit</button>
-    </Link>
+    <div className="confirmation">
+      <Layout>
+        <div>
+          <img src={props.client?.image_url} alt={props.client?.name} />
+          <p>Name: {booking.client}</p>
+          <p>Appointment: {booking.date?.toString()}</p>
+          <p>Service: {booking?.service}</p>
+          <Link to={`/booking/${id}/edit`}>
+            <button>Edit</button>
+          </Link>
+        </div>
+      </Layout>
     </div>
   );
 };
